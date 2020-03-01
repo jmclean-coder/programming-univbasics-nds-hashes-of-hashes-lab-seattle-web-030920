34058c4f28df77b6943b8d8e95a0f354e0e66847
Joshua Mclean <joshmcleana@gmail.com>
2020-03-01 11:00:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@ PORTION_4 = {
 }
 
 def naming_system
-  naming_system = PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  naming_system = PORTION_1
   # Remember:
   #  Kingdom
   #  Phylum
