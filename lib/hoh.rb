281b59557180f305c43d3dc76ff3f1b3c9cd9722
Joshua Mclean <joshmcleana@gmail.com>
2020-03-01 11:45:31 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,9 +31,10 @@ PORTION_4 = {
 }
 
 def naming_system
-hoh = (PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2 PORTION_2[:sub_category] = PORTION_3 PORTION_3[:sub_category][:sub_category] = PORTION_4)
-
-hoh
+PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+PORTION_2[:sub_category] = PORTION_3
+PORTION_3[:sub_category][:sub_category] = PORTION_4
+PORTION_1
   # Remember:
   #  Kingdom
   #  Phylum
